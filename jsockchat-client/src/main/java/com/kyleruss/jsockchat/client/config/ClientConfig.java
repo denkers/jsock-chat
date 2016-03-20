9404f179e56b8f8a7952b0f4b7b6882396deb1fb
denkers <kyle.business@inbox.com>
2016-03-20 22:12:30 +1300

    Added config/ClientConfig, gui/MainPanel in jsockchat-client

---
+++ jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/config/ClientConfig.java
@@ -0,0 +1,7 @@
+
+package com.kyleruss.jsockchat.client.config;
+
+public class ClientConfig 
+{
+    
+}
---
+++ jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/gui/MainPanel.java
@@ -0,0 +1,7 @@
+
+package com.kyleruss.jsockchat.client.gui;
+
+public class MainPanel 
+{
+    
+}
