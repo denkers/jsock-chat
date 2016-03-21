d1362dd2528bf3f2898abd4065ad0ba5a7f7e0db
denkers <kyle.business@inbox.com>
2016-03-21 16:17:07 +1300

    Added singleton patern to core/RoomManger & core/UserManager in jsockchat-server

--- jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/core/RoomManager.java
+++ jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/core/RoomManager.java
@@ -1,8 +1,19 @@
 
-package com.kyleruss.jsockchat.server.io;
+package com.kyleruss.jsockchat.server.core;
 
 
-public class RoomManager
+public final class RoomManager
 {
+    private static RoomManger instance;
     
+    private RoomManager()
+    {
+        
+    }
+    
+    public static RoomManager getInstance()
+    {
+        if(instance == null) instance = new RoomManager();
+        return instance;
+    }
 }
--- jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/core/UserManager.java
+++ jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/core/UserManager.java
@@ -1,7 +1,18 @@
 
 package com.kyleruss.jsockchat.server.core;
 
-public class UserManager
+public final class UserManager
 {
+    private static UserManager instance;
     
+    private UserManager()
+    {
+        
+    }
+    
+    public static UserManager getInstance()
+    {
+        if(instance == null) instance = new UserManager();
+        return instance;
+    }
 }
