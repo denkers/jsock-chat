25451cbfd06dc9fc7151deb5d7ea1e198b783895
denkers <kyle.business@inbox.com>
2016-03-21 17:33:14 +1300

    Added collection impl for Map rooms field in core/RoomManager jsockchat-server

--- jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/core/RoomManager.java
+++ jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/core/RoomManager.java
@@ -1,14 +1,19 @@
 
 package com.kyleruss.jsockchat.server.core;
 
+import com.kyleruss.jsockchat.commons.room.Room;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class RoomManager
 {
-    private static RoomManger instance;
+    private static RoomManager instance;
+    private final Map<String, Room> rooms;
     
     private RoomManager()
     {
-        
+        rooms   =   new HashMap<>();
     }
     
     public static RoomManager getInstance()
@@ -16,4 +21,26 @@ public final class RoomManager
         if(instance == null) instance = new RoomManager();
         return instance;
     }
+    
+    public synchronized boolean addRoom(String roomName, Room room)
+    {
+        return rooms.put(roomName, room) != null;
+    }
+    
+    public synchronized Room removeRoom(String roomName)
+    {
+        return rooms.remove(roomName);
+    }
+    
+    public Room getRoom(String roomName)
+    {
+        return rooms.get(roomName);
+    }
+    
+    public Collection<Room> getRooms()
+    {
+        return rooms.values();
+    }
+    
+    
 }
