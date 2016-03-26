c19b416312c954a24be6a3bb6b8d16ed3ddbd7a1
denkers <kyle.business@inbox.com>
2016-03-27 00:02:50 +1300

    Added jsockchat-commons dependency, updated io/RequestSender with requestQueue field for jsockchat-client

--- jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/io/RequestSender.java
+++ jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/io/RequestSender.java
@@ -1,22 +1,51 @@
 
 package com.kyleruss.jsockchat.client.io;
 
+import com.kyleruss.jsockchat.commons.message.RequestMessage;
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RequestSender extends Thread
 {
     private static RequestSender instance;
     private final Socket socket;
-    
+    private final Queue<RequestMessage> requestQueue;
+            
     private RequestSender(Socket socket)
     {
-        this.socket =   socket;
+        this.socket     =   socket;
+        requestQueue    =   new LinkedList<>();   
+    }
+    
+    private synchronized void getLock()
+    {
+        try
+        {
+            if(requestQueue.isEmpty())
+                wait();
+        }
+
+        catch(InterruptedException e)
+        {
+            System.out.println("[RequestSender@getLock]: " + e.getMessage());
+        }
+    }
+    
+    public synchronized void addRequest(RequestMessage request)
+    {
+        requestQueue.add(request);
+        notify();
     }
     
     @Override
     public void run()
     {
-        
+        while(socket != null && !socket.isClosed())
+        {
+            getLock();
+            RequestMessage nextRequest  =   requestQueue.poll();
+        }
     }
     
     public static RequestSender getInstance(Socket socket)
