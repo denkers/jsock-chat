bda1e2bd2b9ed38c75c3dee5fe3a4e7580f45a09
denkers <kyle.business@inbox.com>
2016-03-26 22:07:37 +1300

    Added io/MessageListener & updated config/ClientConfig with io constants for jsokchat-client

--- jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/config/ClientConfig.java
+++ jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/config/ClientConfig.java
@@ -3,5 +3,10 @@ package com.kyleruss.jsockchat.client.config;
 
 public class ClientConfig 
 {
-    
+    //-------------------------------------
+    //  IO CONSTS
+    //-------------------------------------
+    public static int UPDATE_LIST_PORT      =   8891;
+    public static int MSG_SERVER_PORT       =   8890;
+    public static String MSG_SERVER_HOST    =   "localhost";
 }
---
+++ jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/io/MessageListener.java
@@ -0,0 +1,20 @@
+
+package com.kyleruss.jsockchat.client.io;
+
+import java.net.Socket;
+
+public class MessageListener extends Thread
+{
+    private Socket socket;
+    
+    private void initSocket()
+    {
+        
+    }
+    
+    @Override
+    public void run()
+    {
+        
+    }
+}
