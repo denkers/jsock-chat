20d1fac47615c6ec5c73effb4ac5e31e15496cd8
denkers <kyle.business@inbox.com>
2016-03-27 19:38:18 +1300

    Added AbstractMessage and RequestMessage implementation in message/ClientMessage for jsockchat-client

--- jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/message/ClientMessage.java
+++ jsockchat-client/src/main/java/com/kyleruss/jsockchat/client/message/ClientMessage.java
@@ -3,7 +3,29 @@ package com.kyleruss.jsockchat.client.message;
 import com.kyleruss.jsockchat.commons.message.AbstractMessage;
 import com.kyleruss.jsockchat.commons.message.RequestMessage;
 
-public class ClientMessage extends AbstractMessage implements RequestMessage
+public abstract class ClientMessage extends AbstractMessage implements RequestMessage
 {
+    private String source;
     
+    public abstract void clientAction();
+    
+    public abstract void witnessAction();
+    
+    @Override
+    public String getUserSource() 
+    {
+        return source;
+    }
+
+    @Override
+    public boolean hasSource() 
+    {
+        return source != null;
+    }
+    
+    @Override
+    public boolean isWitness(String username)
+    {
+        return source == null || source.equals(username);
+    }
 }
--- jsockchat-commons/src/main/java/com/kyleruss/jsockchat/commons/message/RequestMessage.java
+++ jsockchat-commons/src/main/java/com/kyleruss/jsockchat/commons/message/RequestMessage.java
@@ -3,13 +3,9 @@ package com.kyleruss.jsockchat.commons.message;
 
 public interface RequestMessage extends Message
 {
-    public int getRequestID();
-    
     public String getUserSource();
-    
-    public boolean isIDSet();
     
     public boolean hasSource();
     
-    public void witnessAction();
+    public boolean isWitness(String username);
 }
