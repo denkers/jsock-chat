cc95c2c8a3447985d5df016742c0df4c295f211b
denkers <kyle.business@inbox.com>
2016-03-28 15:35:58 +1300

    Added message/RegisterMessage interface for jsockchat-commons

---
+++ jsockchat-commons/src/main/java/com/kyleruss/jsockchat/commons/message/RegisterMessage.java
@@ -0,0 +1,11 @@
+
+package com.kyleruss.jsockchat.commons.message;
+
+public interface RegisterMessage 
+{
+    public String getUsername();
+    
+    public String getPassword();
+    
+    public String getDisplayName();
+}
