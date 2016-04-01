97260537e6b41eb512181561c7264cbd28d2f531
denkers <kyle.business@inbox.com>
2016-04-01 17:30:01 +1300

    Fixed up room tree layout splitting with user panel in gui/ServerPanel for jsockchat-server

--- jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/gui/ServerPanel.java
+++ jsockchat-server/src/main/java/com/kyleruss/jsockchat/server/gui/ServerPanel.java
@@ -3,10 +3,12 @@ package com.kyleruss.jsockchat.server.gui;
 import com.kyleruss.jsockchat.commons.room.Room;
 import com.kyleruss.jsockchat.commons.room.RoomTree;
 import com.kyleruss.jsockchat.commons.user.User;
+import com.kyleruss.jsockchat.server.core.RoomManager;
 import com.kyleruss.jsockchat.server.core.ServerConfig;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.BorderFactory;
@@ -20,8 +22,10 @@ public class ServerPanel extends JPanel
     private final LoggingList loggingList;
     private final ServerStatusPanel statusPanel;
     private final JScrollPane loggingScrollPane;
-    private final JPanel leftTreePanel, rightServerPanel;
+    private final JPanel leftPanel, rightPanel;
     private final JSplitPane treeServerSplit;
+    private final JPanel userPanel;
+    private final RoomTree roomTree;
     
     public ServerPanel()
     {
@@ -31,16 +35,35 @@ public class ServerPanel extends JPanel
         loggingList         =   LoggingList.getInstance();
         statusPanel         =   ServerStatusPanel.getInstance();
         loggingScrollPane   =   new JScrollPane(loggingList);
-        leftTreePanel       =   new JPanel(new BorderLayout());
-        rightServerPanel    =   new JPanel(new BorderLayout());
+        leftPanel           =   new JPanel(new GridLayout(2, 1));
+        rightPanel          =   new JPanel(new BorderLayout());
         treeServerSplit     =   new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        userPanel           =   new JPanel();
+        roomTree            =   new RoomTree(new ArrayList<>(RoomManager.getInstance().getDataValues()), "JSockchat Server");
         
+        AppResources resources  =   AppResources.getInstance();
+        roomTree.setServerIcon(new ImageIcon(resources.getServerImage()));
+        roomTree.setRoomIcon(new ImageIcon(resources.getChatImage()));
+        roomTree.setLockedRoomIcon(new ImageIcon(resources.getLockedChatImage()));
+        roomTree.setUserIcon(new ImageIcon(resources.getUserImage()));
         
-        leftTreePanel.setPreferredSize(new Dimension(350, 0));
-        rightServerPanel.add(statusPanel, BorderLayout.NORTH);
-        rightServerPanel.add(loggingScrollPane, BorderLayout.CENTER);
-        treeServerSplit.setLeftComponent(leftTreePanel);
-        treeServerSplit.setRightComponent(rightServerPanel);
+        JPanel treeWrapper          =   new JPanel(new BorderLayout());
+        JPanel leftContentWrapper   =   new JPanel(new BorderLayout());
+        
+        treeWrapper.setBorder(BorderFactory.createTitledBorder("Rooms"));
+        userPanel.setBorder(BorderFactory.createTitledBorder("Online users"));
+        leftContentWrapper.setBorder(BorderFactory.createEmptyBorder(8, 3, 3, 3));
+        leftContentWrapper.setPreferredSize(new Dimension(350, 0));
+        
+        treeWrapper.add(roomTree);
+        leftPanel.add(treeWrapper);
+        leftPanel.add(userPanel);
+        leftContentWrapper.add(leftPanel);
+        
+        rightPanel.add(statusPanel, BorderLayout.NORTH);
+        rightPanel.add(loggingScrollPane, BorderLayout.CENTER);
+        treeServerSplit.setLeftComponent(leftContentWrapper);
+        treeServerSplit.setRightComponent(rightPanel);
         
         add(treeServerSplit);
     }
